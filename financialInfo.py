f192ab8ff32d12e537b6370fd1964c2f6cfa2949
Ashwini Talele <ashtalele70@gmail.com>
2020-04-20 16:56:39 -0700

    Final Commit

--- financialInfo.py
+++ financialInfo.py
@@ -9,6 +9,11 @@ class FinancialInformation:
         self.result = {
             "symbol": "",
             "Organization": "",
+            "error": "",
+            "TimeDate": "",
+            "priceOfStock": "",
+            "changeInPrice": "",
+            "percentChangeInPrice": "",
         }
 
     def getFinanceInformation(self, data):
@@ -21,25 +26,48 @@ class FinancialInformation:
         getSymbolResponse = json.loads(
                 requests.request("GET", getSymbolLink).text)
 
+        try:
+            MachingResponse = getSymbolResponse["bestMatches"]
+            if (not MachingResponse or
+                    (MachingResponse and MachingResponse[0]["9. matchScore"] != "1.0000")):
+                error = "Stock Symbol Not Found"
+                print(error)
+                self.result["error"] = error
+                return self.result
+            else:
+                Organization = MachingResponse[0]["2. name"]
+                try:
+                    getQuoteResponse = json.loads(
+                        requests.request("GET", getQuoteLink).text)["Global Quote"]
+                except:
+                    error = "Failed to connect to Internet to get the Price Quote. Please try again"
+                    print(error)
+                    self.result["error"] = error
+                    return self.result
+                year, month, date = map(str, time.strftime("%Y %m %d").split())
+                TimeDate = datetime.today().strftime('%a') + " " + datetime.now(
+                ).strftime("%b") + " " + date + " " + str(
+                    datetime.now().time()) + " " + time.tzname[1] + " " + year
 
-        MachingResponse = getSymbolResponse["bestMatches"]
-        if (not MachingResponse or
-                (MachingResponse and MachingResponse[0]["9. matchScore"] != "1.0000")):
-            print("Stock Symbol Not Found")
-        else:
-            Organization = MachingResponse[0]["2. name"]
-            try:
-                getQuoteResponse = json.loads(
-                    requests.request("GET", getQuoteLink).text)["Global Quote"]
-            except:
-                print("Failed to connect to Internet to get the Price Quote. Please try again")
-            year, month, date = map(str, time.strftime("%Y %m %d").split())
-            TimeDate = datetime.today().strftime('%a') + " " + datetime.now(
-            ).strftime("%b") + " " + date + " " + str(
-                datetime.now().time()) + " " + time.tzname[1] + " " + year
-
-        #assigning the values back to class variables
-        self.result["symbol"] = symbol
-        self.result["Organization"] = Organization
-
-        return self.result
+                changeInPrice = getQuoteResponse["09. change"]
+                priceOfStock = getQuoteResponse["05. price"]
+                percentChangeInPrice = getQuoteResponse["10. change percent"]
+                if int(float(changeInPrice)) > 0:
+                    changeInPrice = changeInPrice
+                    percentChangeInPrice = percentChangeInPrice
+                else:
+                    changeInPrice = changeInPrice
+                    percentChangeInPrice = percentChangeInPrice
+            #assigning the values back to class variables
+            self.result["symbol"] = symbol
+            self.result["Organization"] = Organization
+            self.result["TimeDate"] = TimeDate
+            self.result["priceOfStock"] = priceOfStock
+            self.result["changeInPrice"] = changeInPrice
+            self.result["percentChangeInPrice"] = percentChangeInPrice
+            return self.result
+        except:
+            error = "Please type valid symbol"
+            print(error)
+            self.result["error"] = error
+            return self.result
